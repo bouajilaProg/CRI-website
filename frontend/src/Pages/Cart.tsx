938bc6a2e428fc09654d2ca8c5b47db3e384f67c
bouajilaProg <bouajilamedyessine@gmail.com>
2024-10-15 22:52:59 +0100

    fixed error with layout + added the side menu

--- frontend/src/Pages/Cart.tsx
+++ frontend/src/Pages/Cart.tsx
@@ -1,12 +1,11 @@
 import CartList from '@/components/Cart/CartList'
 import SideMenu from '@/components/Cart/SideMenu'
-import React from 'react'
 
 function Cart() {
   return (
     <div>
-      <h1 className="text-center text-4xl">Cart</h1>
-      <div className="flex justify-between flex-row md:flex-col gap-4 m-16 ">
+      <h1 className="text-center text-4xl">order List</h1>
+      <div className="flex justify-between flex-col md:flex-row gap-4 m-16 ">
         <CartList />
         <SideMenu />
       </div>
--- frontend/src/components/Cart/SideMenu.tsx
+++ frontend/src/components/Cart/SideMenu.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
 
 function SideMenu() {
   return (
     <div className='flex-1 shadow-black '>
-
+      <div className='card card-side bg-base-100 rounded-none border-b my-2 sm:w-full '>
+        <form className='card-body'>
+          <h2 className='card-title'>send </h2>
+          <label htmlFor="returnDate">Return Date</label>
+          <input type="date" name='ReturnDate' className="input input-bordered" placeholder="date" />
+          <label htmlFor="reason">Reason of order</label>
+          <textarea name='reason' className="textarea h-24 textarea-bordered" placeholder="Reason of order"></textarea>
+          <button className="btn">Send</button>
+        </form>
+      </div>
     </div>
   )
 }
