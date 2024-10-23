3ec29ad807a2e5821fe0a7a28053a998cf90b737
bouajilaProg <bouajilamedyessine@gmail.com>
2024-10-23 15:32:20 +0100

    now the search bar autocomplete takes you to the pages

--- frontend/src/Pages/RTMain.tsx
+++ frontend/src/Pages/RTMain.tsx
@@ -1,38 +1,35 @@
-import FilterBar from "@/components/RT/FilterBar"
-import MaterialList from "@/components/RT/MaterialList"
-import RtSearchBar from "@/components/RT/RtSearchBar"
-import getUserId from "@/utils/UserManager"
-import axios from "axios"
-import { useEffect, useState } from "react"
+import FilterBar from "@/components/RT/FilterBar";
+import MaterialList from "@/components/RT/MaterialList";
+import RtSearchBar from "@/components/RT/RtSearchBar";
+import getUserId from "@/utils/UserManager";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 interface Filters {
-  name: string,
-  category: string[],
-  availability: boolean
+  name: string;
+  category: string[];
+  availability: boolean;
 }
 
 function RTMain() {
   const [filters, setFilters] = useState<Filters>({
     name: "",
     category: [],
-    availability: true
-  })
-
+    availability: true,
+  });
 
   function nameSetter(name: string) {
-    setFilters({ ...filters, name: name })
+    setFilters({ ...filters, name });
   }
 
   function categorySetter(category: string[]) {
-    setFilters({ ...filters, category: category })
+    setFilters({ ...filters, category });
   }
 
   function availabilitySetter(availability: boolean) {
-    setFilters({ ...filters, availability: availability })
+    setFilters({ ...filters, availability });
   }
 
-
-  //types
   type Materiel = {
     materiel_id: number;
     materiel_name: string;
@@ -43,84 +40,87 @@ function RTMain() {
     category_name_id: number;
     category_name: string;
     materiel_qte: number;
+    link: string; // Assuming you have a link property
   };
-
 
   const [categories, setCategories] = useState<string[]>([]);
   const [materielList, setMaterielList] = useState<Materiel[]>([]);
 
   async function onStart() {
-
-    const res = await axios.get("http://localhost:4000/order/current?userid=" + getUserId())
+    const res = await axios.get("http://localhost:4000/order/current?userid=" + getUserId());
 
     if (!res.data) {
-      await axios.post("http://localhost:4000/order/new", { userid: getUserId() })
+      await axios.post("http://localhost:4000/order/new", { userid: getUserId() });
     } else {
-      console.log("already have order")
+      console.log("already have order");
     }
 
-    await axios.get(`http://localhost:4000/materiel/search`)
-      .then((res) => {
-        console.log(res.data);
-        setMaterielList(res.data.materiels);
-        setCategories(res.data.categories);
-      });
+    await axios.get(`http://localhost:4000/materiel/search`).then((res) => {
+      console.log(res.data);
+      setMaterielList(res.data.materiels);
+      setCategories(res.data.categories);
+    });
   }
 
   useEffect(() => {
-    onStart()
-
-  }, []); // Runs only on mount
+    onStart();
+  }, []);
 
   useEffect(() => {
-    //TODO: fetch data from server
     axios.get("http://localhost:4000/materiel/search/" + filters.name).then((res) => {
       setCategories(res.data.categories);
       setMaterielList(res.data.materiels);
-    })
-  }, [filters.name])
+    });
+  }, [filters.name]);
 
   useEffect(() => {
-    //TODO: fetch data from server
     const { category, availability } = filters;
     axios.get(`http://localhost:4000/materiel/search/` + filters.name).then((res) => {
-
       let materielList = res.data.materiels;
-      if (category.length != 0) {
+      if (category.length !== 0) {
         materielList = materielList.filter((materiel: Materiel) => filters.category.includes(materiel.category_name));
       }
 
-      if (availability == true) {
+      if (availability) {
         materielList = materielList.filter((materiel: Materiel) => materiel.materiel_qte > 0);
       }
 
       setMaterielList(materielList);
-    }
-    )
-  }, [filters.category, filters.availability])
+    });
+  }, [filters.category, filters.availability]);
 
-  if (materielList.length == -3) {
+  if (materielList.length === 0) {
     return (
       <>
-        <RtSearchBar nameSetter={nameSetter} />
-        <div className=""></div>
+        <RtSearchBar nameSetter={nameSetter} materiel={materielList.map(materiel => (
+          {
+            name: materiel.materiel_name,
+            description: materiel.description,
+            category: materiel.category_name,
+          }
+        ))} autoComplete="off" />
         <div className="flex flex-wrap sm:flex-row flex-col ">
           <h1 className="text-4xl text-center h-full w-full">No materiel found</h1>
         </div>
       </>
-    )
+    );
   }
 
   return (
     <>
-      <RtSearchBar nameSetter={nameSetter} />
+      <RtSearchBar nameSetter={nameSetter} materiel={materielList.map(materiel => (
+        {
+          name: materiel.materiel_name,
+          id: materiel.materiel_id,
+        }
+      ))} />
       <div className="h-16"></div>
       <div className="flex flex-wrap sm:flex-row flex-col ">
         <FilterBar categorySetter={categorySetter} availabilitySetter={availabilitySetter} categories={categories} />
         <MaterialList materials={materielList} />
       </div>
     </>
-  )
+  );
 }
 
-export default RTMain
+export default RTMain;
--- frontend/src/components/RT/RtSearchBar.tsx
+++ frontend/src/components/RT/RtSearchBar.tsx
@@ -1,86 +1,100 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
+interface RtSearchBarProps {
+  nameSetter: (name: string) => void;
+  materiel: { name: string; id: number }[];
+}
 
-/*interface RtSearchBarProps {
-  searchTerm: string
-  setSearchTerm: (searchTerm: string) => void
-}*/
-
-import materiel from "../../temp/materiel"
-import { useEffect, useState } from "react"
-
-
-function RtSearchBar({ nameSetter }: { nameSetter: (name: string) => void }) {
-  const [search, setSearch] = useState("")
-  let options: string[] = []
+function RtSearchBar({ nameSetter, materiel }: RtSearchBarProps) {
+  const [search, setSearch] = useState("");
+  const [options, setOptions] = useState<{ name: string; id: number }[]>([]);
 
   useEffect(() => {
-    let SearchInput: HTMLInputElement;
-    options = materiel.map(m => m.name)
-    try {
-      SearchInput = document.querySelector("#searchInput") as HTMLInputElement;
-      SearchInput.addEventListener("blur", () => {
-        const autoComplete = document.querySelector("#autoComplete") as HTMLUListElement;
-        autoComplete.classList.add("hidden")
-      })
-      SearchInput.addEventListener("focus", () => {
-        const autoComplete = document.querySelector("#autoComplete") as HTMLUListElement;
-        autoComplete.classList.remove("hidden")
-      })
-    } catch (e) {
-      console.log(e)
-    }
+    const SearchInput = document.querySelector<HTMLInputElement>("#searchInput");
+
+    const handleBlur = () => {
+      const autoComplete = document.querySelector<HTMLUListElement>("#autoComplete");
+      if (autoComplete) {
+        autoComplete.classList.add("hidden");
+      }
+    };
+
+    const handleFocus = () => {
+      const autoComplete = document.querySelector<HTMLUListElement>("#autoComplete");
+      if (autoComplete) {
+        autoComplete.classList.remove("hidden");
+      }
+    };
+
+
+
     return () => {
-      SearchInput.removeEventListener("blur", () => { })
-      SearchInput.removeEventListener("focus", () => { })
-        , []
+      if (SearchInput) {
+        SearchInput.removeEventListener("blur", handleBlur);
+        SearchInput.removeEventListener("focus", handleFocus);
+      }
+    };
+  }, []);
 
-    }
-  })
+  // Fetch options based on search term
+  useEffect(() => {
+    const filteredOptions = materiel.filter(o =>
+      o.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setOptions(filteredOptions);
+  }, [search, materiel]);
 
-  //TODO: the search refetch data only if the search term is different from the previous one
   function handleSearch(e: React.ChangeEvent<HTMLInputElement>) {
-    if (e.target.value.includes(search) == false) {
-      //fetch data
-      options = materiel.map(m => m.name).filter(o => o.includes(search))
-    }
-    setSearch(e.target.value)
-    nameSetter(e.target.value)
+    setSearch(e.target.value);
+    nameSetter(e.target.value);
   }
 
-  options = materiel.map(m => m.name).filter(o => o.includes(search))
-  if (search === "") {
-    options = []
-  }
   return (
-    <div className="ml-4 mr-8 ">
-      <div className="flex justify-center mt-8 ">
-        <label className=" w-full md:w-1/2  p-0 my-0 flex items-center gap-2 gap-y-0 border border-slate-300 rounded-t">
-          <input type="text" className="grow p-2  focus:outline-none border-0" placeholder="Search" value={search} onChange={handleSearch} id="searchInput" />
-          <button className=" p-2 h-full rounded-xs ">
+    <div className="ml-4 mr-8">
+      <div className="flex justify-center mt-8">
+        <label className="w-full md:w-1/2 p-0 my-0 flex items-center gap-2 gap-y-0 border border-slate-300 rounded-t">
+          <input
+            type="text"
+            className="grow p-2 focus:outline-none border-0"
+            placeholder="Search"
+            value={search}
+            onChange={handleSearch}
+            id="searchInput"
+          />
+          <button className="p-2 h-full rounded-xs">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 16 16"
               fill="currentColor"
-              className="h-4 w-4 opacity-70">
+              className="h-4 w-4 opacity-70"
+            >
               <path
                 fillRule="evenodd"
                 d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                clipRule="evenodd" />
+                clipRule="evenodd"
+              />
             </svg>
-
           </button>
         </label>
       </div>
 
       <div className="flex justify-center align-top my-0 relative md:mx-0">
-        <ul id="autoComplete" className={"bg-white list-none text-left w-full md:w-1/2 border z-20 border-t-0 border-slate-300 rounded-b m-0 absolute "} >
-          {options.map((o, i) => (
-            <li key={i} className="p-2 hover:bg-slate-300"><button className="w-full text-left">{o}</button></li>
+        <ul
+          id="autoComplete"
+          className={`bg-white list-none text-left w-full md:w-1/2 border z-20 border-t-0 border-slate-300 rounded-b m-0 absolute ${options.length === 0 ? 'hidden' : ''} ${search.length === 0 ? 'hidden' : ''}`}
+        >
+          {options.map((o) => (
+            <li key={o.id} className="p-2 hover:bg-slate-300">
+              <Link to={`/RT/${o.id}`} className="w-full block text-left">
+                {o.name}
+              </Link>
+            </li>
           ))}
         </ul>
-      </div >
+      </div>
     </div>
-  )
+  );
 }
 
-export default RtSearchBar
+export default RtSearchBar;
